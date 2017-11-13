f7a4b3a18029783f68aad4512471966da8b5ca1e
BeINone <sbeinone@gmail.com>
2017-11-13 22:28:37 +0900

    Added dependencies of mybatis

--- src/main/java/com/depromeet/hay/HayApplication.java
+++ src/main/java/com/depromeet/hay/HayApplication.java
@@ -1,9 +1,13 @@
 package com.depromeet.hay;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class HayApplication {
 
 	public static void main(String[] args) {
